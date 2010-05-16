fab7c4c82354a42565457f34d7b896fcb84f8201
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-05-16 15:18:10 +0200

    Minor style updates.

--- js/bindings.js
+++ js/bindings.js
@@ -17,12 +17,14 @@ function setupKeyBindings ()
      {
        switch (e.charCode)
        {
-         case 91: prevSlide() ; break
-         case 93: nextSlide() ; break
-         case 45: zoomOut()   ; break
-         case 61: zoomIn()    ; break
-         case 48: zoomReset() ; break
+         case 91:  prevSlide()                ; break
+         case 93:  nextSlide()                ; break
+         case 45:  zoomOut()                  ; break
+         case 61:  zoomIn()                   ; break
+         case 48:  zoomReset()                ; break
+         case 115: $(".slidenumber").toggleClass("visible") ; break // s
        }
+       // console.log(e.charCode)
      })
 }
 
