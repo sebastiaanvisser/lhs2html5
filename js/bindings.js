e18e579334c260b90998909b6b7bef390a933cc9
Sebastiaan Visser <sfvisser@cs.uu.nl>
2010-05-18 12:31:38 +0200

    Insert mode.

--- js/bindings.js
+++ js/bindings.js
@@ -3,6 +3,8 @@ function setupKeyBindings ()
   $(document).keydown
     (function (e)
      {
+       if ($(document.body).attr("class").match(/insert/)) return
+
        switch (e.keyCode)
        {
          case 37: prevSlide()  ; break
@@ -15,14 +17,23 @@ function setupKeyBindings ()
   $(document).keypress
     (function (e)
      {
+       if (e.charCode == 105) // i
+       {
+         $("body").toggleClass("insert")
+         return
+       }
+
+       if ($(document.body).attr("class").match(/insert/)) return
+
        switch (e.charCode)
        {
-         case 91:  prevSlide()                ; break
-         case 93:  nextSlide()                ; break
-         case 45:  zoomOut()                  ; break
-         case 61:  zoomIn()                   ; break
-         case 48:  zoomReset()                ; break
+         case 91:  prevSlide()                              ; break
+         case 93:  nextSlide()                              ; break
+         case 45:  zoomOut()                                ; break
+         case 61:  zoomIn()                                 ; break
+         case 48:  zoomReset()                              ; break
          case 115: $(".slidenumber").toggleClass("visible") ; break // s
+         case 109: $("body").toggleClass("mouse")           ; break // m
        }
        // console.log(e.charCode)
      })
@@ -30,19 +41,18 @@ function setupKeyBindings ()
 
 function setupMouseBindings ()
 {
-  if (document.location.search.match(/nomouse/)) return
-
   // Disable context menu.
-  $(document).bind("contextmenu",function() { return false })
-
-  // Make stuff unselectable and hide cursor.
-  $("*").css("-webkit-user-select", "none");
-  $("*").css("cursor", "none");
+  $(document).bind("contextmenu",
+    function ()
+    {
+      return !$(document.body).attr("class").match(/mouse/)
+    })
 
   // Navigate forward on left mouse button, backward on right mouse button.
   $("body").mousedown
     (function (e)
      {
+       if (!$(document.body).attr("class").match(/mouse/)) return
        switch (e.which)
        {
          case 1: nextSlide() ; break
